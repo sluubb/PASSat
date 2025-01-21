818d0df2dae10d13718b1d8a40bf18c36a74c843
SlubblesThuggy <65078309+SlubblesThuggy@users.noreply.github.com>
2025-01-21 22:06:24 +0100

    fix: modify component init
    Modify component initialization so that it retries upon error.
    Also slight refactor of logging functions.

--- pasat.ino
+++ pasat.ino
@@ -2,13 +2,16 @@
 #include <SD.h>
 
 #include <DFRobot_ICP10111.h>
+#include <DFRobot_AHT20.h>
 
 #define LOG_FILE "log.txt"
+#define RETRY_DELAY 1000
 
 // 0->don't log to serial | 1->log only info & errors | 2->log all
 #define SERIAL_LOG_LEVEL 2
 
 DFRobot_ICP10111 pressureSensor;
+DFRobot_AHT20 tempHumSensor;
 
 void setup() {
 #if SERIAL_LOG_LEVEL > 0
@@ -17,15 +20,26 @@ void setup() {
   while (!Serial);
 #endif
 
-  if (pressureSensor.begin() != 0) {
+  // initialize pressure sensor
+  while (pressureSensor.begin() != 0) {
     error("Pressure sensor initialization failed.");
+    delay(RETRY_DELAY);
   }
   pressureSensor.setWorkPattern(pressureSensor.eNormal);
   info("Pressure sensor initialized.");
 
+  // initialize pressure sensor
+  uint8_t status;
+  while ((status = tempHumSensor.begin()) != 0) {
+    error("Temperature/humidity sensor initialization failed (status: " + String(status) + ").");
+    delay(RETRY_DELAY);
+  }
+  info("Temperature/humidity sensor initialized.");  
+
   // initialize SD card
-  if (!SD.begin(SDCARD_SS_PIN)) {
+  while (!SD.begin(SDCARD_SS_PIN)) {
     error("SD card initialization failed.");
+    delay(RETRY_DELAY);
   }
   info("SD card initialized.");
 
@@ -37,7 +51,15 @@ void setup() {
 }
 
 void loop() {
-  log(String(pressureSensor.getAirPressure()));
+  if (!tempHumSensor.startMeasurementReady(true)) return;
+
+  log(
+    String(pressureSensor.getAirPressure()) +
+    ", " +
+    String(tempHumSensor.getTemperature_C()) +
+    ", " +
+    String(tempHumSensor.getHumidity_RH())
+  );
 }
 
 
@@ -47,8 +69,11 @@ void log(String s) {
   File log = SD.open(LOG_FILE, FILE_WRITE);
   if (log) {
 #if SERIAL_LOG_LEVEL > 1
-    Serial.println("LOG :: " + s);
+    Serial.println("LOG :: ")
+    Serial.print(s);
 #endif
+    log.print(millis());
+    log.print(" :: ");
     log.println(s);
     log.close();
   } else {
@@ -59,15 +84,14 @@ void log(String s) {
 void error(String s) {
   // TODO: physically indicate
 #if SERIAL_LOG_LEVEL > 0
-  Serial.println("ERROR :: " + s);
+  Serial.print("ERROR :: ");
+  Serial.println(s);
 #endif
-  
-  // errors should end program for safety
-  while (true);
 }
 
 void info(String s) {
 #if SERIAL_LOG_LEVEL > 0
-  Serial.println("INFO :: " + s);
+  Serial.print("INFO :: ");
+  Serial.println(s);
 #endif
 }
